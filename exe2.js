b6735b4f40d4b046d65209872cc06d861b19d8f9
calvinnhs <tugasakun22@gmail.com>
2023-06-19 17:44:42 +0700

    first commit

---
+++ exe2.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video COLDPLAY WORLD TOUR");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio VIVA LA VIDA");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar TEAM COLADPLAY");
+});
